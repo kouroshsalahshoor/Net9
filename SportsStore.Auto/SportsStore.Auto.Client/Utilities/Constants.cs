d8ab98b80bc48803c2f7dbc67dbfdb1e891b2dcf
kourosh <kourosh23@hotmail.com>
2025-02-07 09:28:00 +0100

    user delete

--- SportsStore.Auto/SportsStore.Auto.Client/Utilities/Constants.cs
+++ SportsStore.Auto/SportsStore.Auto.Client/Utilities/Constants.cs
@@ -4,5 +4,7 @@
     {
         public const string Role_Admins = "Admins";
         public const string Role_Users = "Users";
+
+        public const string DeleteText = "Are you sure you want to delete this item?";
     }
 }
