a974d7aef271afb72572b4cf288fb4b9e7fea68f
kourosh <kourosh23@hotmail.com>
2025-02-09 10:22:54 +0100

    product crud

--- SportsStore.Auto/SportsStore.Auto/Services/CartService.cs
+++ SportsStore.Auto/SportsStore.Auto/Services/CartService.cs
@@ -17,7 +17,7 @@ namespace SportsStore.Auto.Services
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
         public virtual async Task AddItem(Product product, int quantity)
         {
-            CartLine? line = Lines.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+            CartLine? line = Lines.Where(p => p.Product?.Id == product.Id).FirstOrDefault();
             if (line == null)
             {
                 Lines.Add(new CartLine
@@ -32,8 +32,8 @@ namespace SportsStore.Auto.Services
                 line.Quantity += quantity;
             }
         }
-        public virtual async Task RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.Id == product.Id);
-        public decimal ComputeTotalValue() => Lines.Sum(e => e.Product.Price * e.Quantity);
+        public virtual async Task RemoveLine(Product product) => Lines.RemoveAll(l => l.Product?.Id == product.Id);
+        public decimal ComputeTotalValue() => Lines.Sum(e => e.Product!.Price * e.Quantity);
         public virtual async Task Clear() => Lines.Clear();
         public virtual async Task Load() { }
         public async Task setSummary()
