a452518bc3a49c83b36f3a84676abc1f1adaeb35
kourosh <kourosh23@hotmail.com>
2025-02-09 10:22:54 +0100

    product crud

--- SportsStore.Auto/SportsStore.Auto.Client/Models/Product.cs
+++ SportsStore.Auto/SportsStore.Auto.Client/Models/Product.cs
@@ -6,15 +6,16 @@ namespace SportsStore.Auto.Client.Models
     public class Product
     {
         public long? Id { get; set; }
-        [Required(ErrorMessage = "Please enter a product name")]
+        [Required(ErrorMessage = "Please enter a product {0}")]
         public string Name { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Please enter a description")]
+        [Required(ErrorMessage = "Please enter a {0}")]
         public string Description { get; set; } = string.Empty;
         [Required]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive {0}")]
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
-        [Required(ErrorMessage = "Please specify a category")]
+        [Required(ErrorMessage = "Please specify a {0}")]
         public string Category { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; }
     }
 }
