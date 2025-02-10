51d315c86dff5bb17d666149a4e2a76e5e054bce
kourosh <kourosh23@hotmail.com>
2025-02-10 12:48:55 +0100

    modals

---
+++ SportsStore.Auto/SportsStore.Auto/Controllers/UploadImageController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Auto.Client.Models;
+using SportsStore.Auto.Data;
+using System.Runtime.CompilerServices;
+
+namespace SportsStore.Auto.Controllers
+{
+    [ApiController]
+    [Route("/api/uploadimage")]
+    public class UploadImageController : ControllerBase
+    {
+        //https://www.youtube.com/watch?v=tESVLxcD6Bw
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private ApplicationDbContext context;
+        public UploadImageController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost("{id}")]
+        public void Upload(long? id, [FromBody] string imageData)
+        {
+            var dir = Path.Combine(_webHostEnvironment.WebRootPath, "images", "products");
+            if (Directory.Exists(dir) == false)
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            var productDir = Path.Combine(dir, id.ToString()!);
+            if (Directory.Exists(productDir) == false)
+            {
+                Directory.CreateDirectory(productDir);
+            }
+
+            var fileName = $"{Guid.NewGuid()}.png";
+            var filePath = Path.Combine(productDir, fileName);
+
+            //var fileInfo = new FileInfo(file.Name);
+
+            //var newFileName = $"{Guid.NewGuid()}{fileInfo.Extension}";
+            //var newFilePath = Path.Combine(productDir, newFileName);
+
+            //await using var fs = new FileStream(newFilePath, FileMode.Create);
+            //await file.OpenReadStream(file.Size).CopyToAsync(fs);
+
+            using var fs = new FileStream(filePath, FileMode.Create);
+            using var bw = new BinaryWriter(fs);
+            var data = Convert.FromBase64String(imageData.Split(",")[1]);
+            bw.Write(data);
+            bw.Close();
+            fs.Close();
+
+            //create/update model
+            //save in db
+
+            //return Json(new {success = true, imagePath = filePath});
+        }
+    }
+}
---
+++ SportsStore.Auto/SportsStore.Auto/wwwroot/js/captureImage.js
@@ -0,0 +1,34 @@
+﻿var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(function (err) {
+        console.log("An error occurred: " + err);
+    });
+
+document.getElementById("captureButton").addEventListener("click", function () {
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    var image = canvas.toDataURL("image/png");
+    uploadImage(image);
+}
+
+function uploadImage(image) {
+        fetch("/api/uploadimage", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ image: image })
+        })
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+            })
+            .catch(function (err) {
+                console.log("An error occurred: " + err);
+            });
+    }
